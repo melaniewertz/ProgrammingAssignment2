215aa3338b5062e79ff3625a61d219af5ac42c57
Melanie Wertz <melaniewertz@Melanies-MacBook-Pro.local>
2015-11-16 13:57:37 -0600

    Testing upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## Adding a comment to test uploading to github
 
 ## Write a short comment describing this function
 
